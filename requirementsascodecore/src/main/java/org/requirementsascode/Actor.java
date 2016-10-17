6d649c1b0e5e26f0f7721c0fdd79349b9b4d0b23
bertilmuth <bertilmuth@yahoo.de>
2016-10-17 22:11:09 +0200

    Added javadoc comments to Actor class

--- requirementsascodecore/src/main/java/org/requirementsascode/Actor.java
+++ requirementsascodecore/src/main/java/org/requirementsascode/Actor.java
@@ -8,6 +8,16 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set; 
 
+/**
+ * An actor, as part of a use case model.
+ * 
+ * An actor is a role that a user plays. Actors in use case models represent different user groups.
+ * They enable to distinguish user rights: only an actor that is connected to a particular use case step
+ * is allowed to cause a system reaction for that step.
+ * 
+ * @author b_muth
+ *
+ */
 public class Actor extends UseCaseModelElement{
 	private Map<UseCase, List<UseCaseStep>> useCaseToStepMap;
 
@@ -16,11 +26,26 @@ public class Actor extends UseCaseModelElement{
 		this.useCaseToStepMap = new HashMap<>();
 	}
 
+	/**
+	 * Returns the use cases this actor is associated with.
+	 * 
+	 * The actor is associated to a use case if it is connected
+	 * to at least one of its use case steps.
+	 * 
+	 * @return the set of use cases the actor is associated with
+	 */
 	public Set<UseCase> getUseCases() {
 		Set<UseCase> useCases = useCaseToStepMap.keySet();
 		return Collections.unmodifiableSet(useCases);
 	}
 
+	/**
+	 * Returns the use case steps this actor is connected with,
+	 * for the specified use case.
+	 * 
+	 * @param useCase the use case to query for steps the actor is connected with
+	 * @return the set of use case steps the actor is connected with
+	 */
 	public List<UseCaseStep> getUseCaseSteps(UseCase useCase) {
 		Objects.requireNonNull(useCase);
 
