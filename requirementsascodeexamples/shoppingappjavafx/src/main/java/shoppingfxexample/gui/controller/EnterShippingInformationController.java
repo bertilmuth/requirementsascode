60eaf260a5cdf06f827e51c9b50231031a7fc592
bertilmuth <bertilmuth@yahoo.de>
2016-10-23 23:08:13 +0200

    Update Shopping Example

--- requirementsascodeexamples/shoppingappjavafx/src/main/java/shoppingfxexample/gui/controller/EnterShippingInformationController.java
+++ requirementsascodeexamples/shoppingappjavafx/src/main/java/shoppingfxexample/gui/controller/EnterShippingInformationController.java
@@ -1,10 +1,9 @@
 package shoppingfxexample.gui.controller;
 
-import com.dooapp.fxform.FXForm;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import shoppingfxexample.domain.ShippingInformation;
 import shoppingfxexample.usecase.event.CheckoutPurchaseEvent;
@@ -17,20 +16,44 @@ public class EnterShippingInformationController extends AbstractController{
 	
     @FXML
     private Button confirmButton;
+    
+    @FXML
+    private TextField nameField;
+    
+    @FXML
+    private TextField streetField;
+    
+    @FXML
+    private TextField zipField;
+
+    @FXML
+    private TextField cityField;
+
+    @FXML
+    private TextField stateField;
+    
+    @FXML
+    private TextField countryField;
 
 	private ShippingInformation shippingInformation;
     
     @FXML
     void onConfirm(ActionEvent event) {
+    	initShippingInformationFromForm();
     	EnterShippingInformationEvent enterShippingInformation =  new EnterShippingInformationEvent(shippingInformation);
     	getUseCaseRunner().reactTo(enterShippingInformation);
     }
+
+	private void initShippingInformationFromForm() {
+		shippingInformation.setName(nameField.getText());
+    	shippingInformation.setStreet(streetField.getText());
+    	shippingInformation.setZip(zipField.getText());
+    	shippingInformation.setCity(cityField.getText());
+    	shippingInformation.setState(stateField.getText());
+    	shippingInformation.setCountry(countryField.getText());
+	}
     
-	public void enterShippingInformation(CheckoutPurchaseEvent checkoutPurchase) {
+	public void enterShippingInformation(CheckoutPurchaseEvent checkoutPurchaseEvent) {
     	this.shippingInformation = new ShippingInformation();
-    	
-    	FXForm<ShippingInformation> shippingInformationForm = 
-    			new FXForm<>(shippingInformation);
-    	vBox.getChildren().add(shippingInformationForm);
 	}
 }
