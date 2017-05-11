945672273964b79027549c9087a446090733f337
Bertil Muth <bertilmuth@yahoo.de>
2017-05-11 21:54:00 +0200

    Included Basic Flow can be run on its own

--- requirementsascodecore/src/test/java/org/requirementsascode/IncludeTest.java
+++ requirementsascodecore/src/test/java/org/requirementsascode/IncludeTest.java
@@ -6,6 +6,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class IncludeTest extends AbstractTestCase{
+    private static final String INCLUDED_USE_CASE = "Included Use Case";
+
     @Before
     public void setup() {
       setupWith(new TestUseCaseModelRunner());
@@ -14,18 +16,36 @@ public class IncludeTest extends AbstractTestCase{
     @Test
     public void includeBasicFlowAfterFirstStep() {
       UseCaseModel useCaseModel = useCaseModelBuilder
-        .useCase("Included Use Case")
+        .useCase(INCLUDED_USE_CASE)
           .basicFlow()
             .step(SYSTEM_DISPLAYS_NUMBER).user(EnterNumber.class).system(displayEnteredNumber())
         .useCase(USE_CASE)
           .basicFlow()
             .step(SYSTEM_DISPLAYS_TEXT).user(EnterText.class).system(displayEnteredText())
-            .include("Included Use Case")
+            .include(INCLUDED_USE_CASE)
         .build();
       
       useCaseModelRunner.run(useCaseModel);
       useCaseModelRunner.reactTo(enterText(), enterNumber());
       
       assertEquals(SYSTEM_DISPLAYS_TEXT + ";" + SYSTEM_DISPLAYS_NUMBER + ";", runStepNames());
+    }
+    
+    @Test
+    public void includedBasicFlowCanBeRunOnItsOwn() {
+      UseCaseModel useCaseModel = useCaseModelBuilder
+        .useCase(INCLUDED_USE_CASE)
+          .basicFlow()
+            .step(SYSTEM_DISPLAYS_NUMBER).user(EnterNumber.class).system(displayEnteredNumber())
+        .useCase(USE_CASE)
+          .basicFlow()
+            .step(SYSTEM_DISPLAYS_TEXT).user(EnterText.class).system(displayEnteredText())
+            .include(INCLUDED_USE_CASE)
+        .build();
+      
+      useCaseModelRunner.run(useCaseModel);
+      useCaseModelRunner.reactTo(enterNumber());
+      
+      assertEquals(SYSTEM_DISPLAYS_NUMBER + ";", runStepNames());
     }
 }
