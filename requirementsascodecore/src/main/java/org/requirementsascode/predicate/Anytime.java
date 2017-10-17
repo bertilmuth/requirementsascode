b1123d7fbf6496dff48500205d840ab4d8a39b0f
Bertil Muth <bertilmuth@yahoo.de>
2017-10-17 19:30:20 +0200

    Make Anytime class Serializable

--- requirementsascodecore/src/main/java/org/requirementsascode/predicate/Anytime.java
+++ requirementsascodecore/src/main/java/org/requirementsascode/predicate/Anytime.java
@@ -1,10 +1,13 @@
 package org.requirementsascode.predicate;
 
+import java.io.Serializable;
 import java.util.function.Predicate;
 
 import org.requirementsascode.UseCaseModelRunner;
 
-public class Anytime implements Predicate<UseCaseModelRunner> {
+public class Anytime implements Predicate<UseCaseModelRunner>, Serializable {
+  private static final long serialVersionUID = 7724607380865304333L;
+
   @Override
   public boolean test(UseCaseModelRunner t) {
     return true;
