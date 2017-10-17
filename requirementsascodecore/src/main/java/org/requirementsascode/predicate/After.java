269b056277a3e9dbd5906b864ce083f3cfc67e4b
Bertil Muth <bertilmuth@yahoo.de>
2017-10-17 19:29:39 +0200

    Make After class Serializable

--- requirementsascodecore/src/main/java/org/requirementsascode/predicate/After.java
+++ requirementsascodecore/src/main/java/org/requirementsascode/predicate/After.java
@@ -1,12 +1,15 @@
 package org.requirementsascode.predicate;
 
+import java.io.Serializable;
 import java.util.Objects;
 
 import org.requirementsascode.Step;
 import org.requirementsascode.UseCaseModelRunner;
 
-public class After implements FlowPosition{
-	private Step step;
+public class After implements FlowPosition, Serializable{
+  private static final long serialVersionUID = -4951912635216926005L;
+  
+  private Step step;
 
 	public After(Step step) {
 		this.step = step;
