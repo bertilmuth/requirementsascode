07e1abda233079138dd67912d76f02ffd8784999
Bertil Muth <bertilmuth@yahoo.de>
2017-10-17 19:34:39 +0200

    Make ContinueAt class Serializable

--- requirementsascodecore/src/main/java/org/requirementsascode/systemreaction/ContinueAt.java
+++ requirementsascodecore/src/main/java/org/requirementsascode/systemreaction/ContinueAt.java
@@ -1,9 +1,13 @@
 package org.requirementsascode.systemreaction;
 
+import java.io.Serializable;
+
 import org.requirementsascode.UseCase;
 
-public class ContinueAt extends AbstractContinue{
-	public ContinueAt(UseCase useCase, String stepName) {
+public class ContinueAt extends AbstractContinue implements Serializable{
+  private static final long serialVersionUID = -1087791192033164673L;
+
+  public ContinueAt(UseCase useCase, String stepName) {
 		super(stepName, useCase.findStep(stepName).getPreviousStepInFlow());
 	}
 }
