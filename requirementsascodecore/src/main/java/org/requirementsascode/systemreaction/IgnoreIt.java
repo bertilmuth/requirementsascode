5170ae6220c485c88c55b59fe9c43da0ea3c5139
Bertil Muth <bertilmuth@yahoo.de>
2017-10-17 19:35:58 +0200

    Make IgnoreIt class Serializable

--- requirementsascodecore/src/main/java/org/requirementsascode/systemreaction/IgnoreIt.java
+++ requirementsascodecore/src/main/java/org/requirementsascode/systemreaction/IgnoreIt.java
@@ -1,9 +1,12 @@
 package org.requirementsascode.systemreaction;
 
+import java.io.Serializable;
 import java.util.function.Consumer;
 
-public class IgnoreIt<T> implements Consumer<T>{
-	@Override
+public class IgnoreIt<T> implements Consumer<T>, Serializable{
+  private static final long serialVersionUID = 2735467041247660436L;
+
+  @Override
 	public void accept(T event) {
 	}
 }
