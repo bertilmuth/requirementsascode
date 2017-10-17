834197abda84d8e3319403dfa4d9a5b1bd618172
Bertil Muth <bertilmuth@yahoo.de>
2017-10-17 19:34:01 +0200

    Make ContinueAfter class Serializable

--- requirementsascodecore/src/main/java/org/requirementsascode/systemreaction/ContinueAfter.java
+++ requirementsascodecore/src/main/java/org/requirementsascode/systemreaction/ContinueAfter.java
@@ -1,9 +1,13 @@
 package org.requirementsascode.systemreaction;
 
+import java.io.Serializable;
+
 import org.requirementsascode.UseCase;
 
-public class ContinueAfter extends AbstractContinue{
-	public ContinueAfter(UseCase useCase, String stepName) {
+public class ContinueAfter extends AbstractContinue implements Serializable{
+  private static final long serialVersionUID = -8065764394122743979L;
+
+  public ContinueAfter(UseCase useCase, String stepName) {
 		super(stepName, useCase.findStep(stepName));
 	}
 }
