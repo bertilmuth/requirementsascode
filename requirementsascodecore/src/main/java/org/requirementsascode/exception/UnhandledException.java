484141df04bf6b94c4b1de7c79819fb7268493c7
Bertil Muth <bertilmuth@yahoo.de>
2017-10-17 19:28:51 +0200

    Make UnhandledException class Serializable

--- requirementsascodecore/src/main/java/org/requirementsascode/exception/UnhandledException.java
+++ requirementsascodecore/src/main/java/org/requirementsascode/exception/UnhandledException.java
@@ -1,12 +1,14 @@
 package org.requirementsascode.exception;
 
+import java.io.Serializable;
+
 /**
  * Exception that is thrown when a system reaction throws an exception that is not handled.
  * 
  * @author b_muth
  *
  */
-public class UnhandledException extends RuntimeException{
+public class UnhandledException extends RuntimeException implements Serializable{
 	private static final long serialVersionUID = 8510144283265242951L;
 	
 	public UnhandledException(Throwable uncaughtException) {
