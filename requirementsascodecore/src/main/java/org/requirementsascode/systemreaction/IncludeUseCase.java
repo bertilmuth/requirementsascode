dcf9dec0fcf0c753d22f098b42c1fb94ea599402
Bertil Muth <bertilmuth@yahoo.de>
2017-10-17 19:36:51 +0200

    Make IncludeUseCase class Serializable

--- requirementsascodecore/src/main/java/org/requirementsascode/systemreaction/IncludeUseCase.java
+++ requirementsascodecore/src/main/java/org/requirementsascode/systemreaction/IncludeUseCase.java
@@ -1,12 +1,15 @@
 package org.requirementsascode.systemreaction;
 
+import java.io.Serializable;
 import java.util.function.Consumer;
 
 import org.requirementsascode.Step;
 import org.requirementsascode.UseCase;
 import org.requirementsascode.UseCaseModelRunner;
 
-public class IncludeUseCase implements Consumer<UseCaseModelRunner> {
+public class IncludeUseCase implements Consumer<UseCaseModelRunner>, Serializable {
+  private static final long serialVersionUID = -9078568632090369442L;
+  
   private UseCase includedUseCase;
   private Step includeStep;
 
