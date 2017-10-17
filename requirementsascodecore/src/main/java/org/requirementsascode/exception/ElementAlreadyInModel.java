70fecd97391e4e4314173c468200a11d1e075de3
Bertil Muth <bertilmuth@yahoo.de>
2017-10-17 19:26:57 +0200

    Make ElementAlreadyInModel class Serializable

--- requirementsascodecore/src/main/java/org/requirementsascode/exception/ElementAlreadyInModel.java
+++ requirementsascodecore/src/main/java/org/requirementsascode/exception/ElementAlreadyInModel.java
@@ -1,5 +1,7 @@
 package org.requirementsascode.exception;
 
+import java.io.Serializable;
+
 /**
  * Exception that is thrown when somebody tries to create a new model element,
  * and a model element with the same name is already in the model.
@@ -7,7 +9,7 @@ package org.requirementsascode.exception;
  * @author b_muth
  *
  */
-public class ElementAlreadyInModel extends RuntimeException {
+public class ElementAlreadyInModel extends RuntimeException implements Serializable{
 	private static final long serialVersionUID = -510216736346192818L;
 
 	public ElementAlreadyInModel(String elementName) {		
