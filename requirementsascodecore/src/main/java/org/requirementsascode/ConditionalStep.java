fc4852b723dc39828a02d09829a2686f7b2c2ace
Bertil Muth <bertilmuth@yahoo.de>
2018-04-17 16:43:18 +0200

    Remove unnecessary conditional

--- requirementsascodecore/src/main/java/org/requirementsascode/ConditionalStep.java
+++ requirementsascodecore/src/main/java/org/requirementsascode/ConditionalStep.java
@@ -11,35 +11,32 @@ public class ConditionalStep extends Step {
     ConditionalStep(String stepName, UseCase useCase, Flow useCaseFlow) {
 	super(stepName, useCase, useCaseFlow);
     }
-    
+
     public Predicate<UseCaseModelRunner> getPredicate() {
 	Predicate<UseCaseModelRunner> predicate;
 	Predicate<UseCaseModelRunner> reactWhile = getReactWhile();
-	
+
 	if (reactWhile != null) {
 	    predicate = reactWhile;
-	} else{ 
+	} else {
 	    predicate = getFlowPredicate().get();
 	}
 
 	return predicate;
     }
-    
+
     public Optional<Predicate<UseCaseModelRunner>> getFlowPredicate() {
 	Optional<Predicate<UseCaseModelRunner>> flowPredicate = Optional.empty();
 	Optional<Predicate<UseCaseModelRunner>> flowPosition = getFlowPosition();
 	Optional<Predicate<UseCaseModelRunner>> when = getWhen();
-	
-	if (flowPosition.isPresent() || when.isPresent()) {
-	    Anytime anytime = new Anytime();
-	    Predicate<UseCaseModelRunner> flowPositionOrElseAnytime = flowPosition.orElse(anytime);
-	    Predicate<UseCaseModelRunner> whenOrElseAnytime = when.orElse(anytime);
-	    flowPredicate = Optional
-		    .of(isRunnerInDifferentFlow().and(flowPositionOrElseAnytime).and(whenOrElseAnytime));
-	}
+
+	Anytime anytime = new Anytime();
+	Predicate<UseCaseModelRunner> flowPositionOrElseAnytime = flowPosition.orElse(anytime);
+	Predicate<UseCaseModelRunner> whenOrElseAnytime = when.orElse(anytime);
+	flowPredicate = Optional.of(isRunnerInDifferentFlow().and(flowPositionOrElseAnytime).and(whenOrElseAnytime));
 	return flowPredicate;
     }
-    
+
     private Predicate<UseCaseModelRunner> isRunnerInDifferentFlow() {
 	Predicate<UseCaseModelRunner> isRunnerInDifferentFlow = runner -> runner.getLatestFlow()
 		.map(runnerFlow -> !runnerFlow.equals(getFlow())).orElse(true);
