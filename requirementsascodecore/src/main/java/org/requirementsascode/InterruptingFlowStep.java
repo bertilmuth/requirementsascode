49c37e882dc542476042c3f546669830032e5878
Bertil Muth <bertilmuth@yahoo.de>
2019-05-28 11:05:50 +0200

    Revert "Refactor & Layout"
    This reverts commit bb44df42ca81659385cb9f32e0ca1d2480fc370b.

--- requirementsascodecore/src/main/java/org/requirementsascode/InterruptingFlowStep.java
+++ requirementsascodecore/src/main/java/org/requirementsascode/InterruptingFlowStep.java
@@ -5,32 +5,36 @@ import java.util.function.Predicate;
 import org.requirementsascode.flowposition.FlowPosition;
 
 public class InterruptingFlowStep extends FlowStep {
-	private static final long serialVersionUID = 7204738737376844201L;
+    private static final long serialVersionUID = 7204738737376844201L;
 
-	InterruptingFlowStep(String stepName, Flow useCaseFlow, FlowPosition flowPosition, Condition condition) {
-		super(stepName, useCaseFlow, condition);
-		setFlowPosition(flowPosition);
+    InterruptingFlowStep(String stepName, Flow useCaseFlow, FlowPosition flowPosition, Condition condition) {
+	super(stepName, useCaseFlow, condition);
+	setFlowPosition(flowPosition);
+    }
+
+    public Predicate<ModelRunner> getPredicate() {
+	Predicate<ModelRunner> predicate;
+	Condition reactWhile = getReactWhile();
+
+	predicate = isFlowConditionTrueAndRunnerInDifferentFlow();
+	if (reactWhile != null) {
+	    predicate = predicate.and(toPredicate(reactWhile));
 	}
 
-	public Predicate<ModelRunner> getPredicate() {
-		Predicate<ModelRunner> reactWhilePredicate = toPredicate(getReactWhile());
-		Predicate<ModelRunner> predicate = reactWhilePredicate.and(isFlowConditionTrueAndRunnerInDifferentFlow());
+	return predicate;
+    }
 
-		return predicate;
-	}
+    private Predicate<ModelRunner> isFlowConditionTrueAndRunnerInDifferentFlow() {
+	Predicate<ModelRunner> flowPosition = getFlowPosition();
+	Condition conditionOrElseTrue = getCondition().orElse(() -> true);
 
-	private Predicate<ModelRunner> isFlowConditionTrueAndRunnerInDifferentFlow() {
-		Predicate<ModelRunner> flowPosition = getFlowPosition();
-		Condition conditionOrElseTrue = getCondition().orElse(() -> true);
+	Predicate<ModelRunner> flowCondition = isRunnerInDifferentFlow().and(flowPosition).and(toPredicate(conditionOrElseTrue));
+	return flowCondition;
+    }
 
-		Predicate<ModelRunner> flowCondition = isRunnerInDifferentFlow().and(flowPosition)
-				.and(toPredicate(conditionOrElseTrue));
-		return flowCondition;
-	}
-
-	private Predicate<ModelRunner> isRunnerInDifferentFlow() {
-		Predicate<ModelRunner> isRunnerInDifferentFlow = runner -> runner.getLatestFlow()
-				.map(runnerFlow -> !runnerFlow.equals(getFlow())).orElse(true);
-		return isRunnerInDifferentFlow;
-	}
+    private Predicate<ModelRunner> isRunnerInDifferentFlow() {
+	Predicate<ModelRunner> isRunnerInDifferentFlow = runner -> runner.getLatestFlow()
+		.map(runnerFlow -> !runnerFlow.equals(getFlow())).orElse(true);
+	return isRunnerInDifferentFlow;
+    }
 }
