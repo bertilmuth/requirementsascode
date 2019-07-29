8ab106fe760971ca587a53a48b7b07a51c835c9e
Bertil Muth <bertilmuth@yahoo.de>
2019-07-29 15:22:38 +0200

    Simplify example (remove flow)

--- requirementsascodeexamples/crosscuttingconcerns/README.md
+++ requirementsascodeexamples/crosscuttingconcerns/README.md
@@ -1,16 +1,13 @@
 # cross-cutting concerns example 01 - measure performance of system reactions
 ``` java
-ModelBuilder modelBuilder = Model.builder();
-Model model = 
-	modelBuilder.useCase("Measure performance of simple mathematical operations")
-		.basicFlow()
-			.step("S1").system(this::addsTwoNumbers)
-			.step("S2").system(this::calculatesTwoToThePowerOfAThousand)
-			.step("S3").system(this::displaysResults)
-	.build();
+	public void start() {
+		Model model = Model.builder()
+			.user(RequestCalculating.class).system(this::calculate)
+		.build();
 
-ModelRunner modelRunner = new ModelRunner();
-modelRunner.handleWith(this::measuresPerformance);
-modelRunner.run(model);
+		ModelRunner modelRunner = new ModelRunner();
+		modelRunner.handleWith(this::measuresPerformance);
+		modelRunner.run(model).reactTo(new RequestCalculating());
+	}
 ```
 For the full source code, [look here](https://github.com/bertilmuth/requirementsascode/blob/master/requirementsascodeexamples/crosscuttingconcerns/src/main/java/crosscuttingconcerns/CrossCuttingConcerns01.java).
--- requirementsascodeexamples/crosscuttingconcerns/src/main/java/crosscuttingconcerns/CrossCuttingConcerns01.java
+++ requirementsascodeexamples/crosscuttingconcerns/src/main/java/crosscuttingconcerns/CrossCuttingConcerns01.java
@@ -1,49 +1,37 @@
 package crosscuttingconcerns;
 
 import org.requirementsascode.Model;
-import org.requirementsascode.ModelBuilder;
 import org.requirementsascode.ModelRunner;
 import org.requirementsascode.StepToBeRun;
 
 public class CrossCuttingConcerns01 {
-    private int resultOfAddition;
-    private double resultOfPower;
+	public void start() {
+		Model model = Model.builder()
+			.user(RequestCalculating.class).system(this::calculate)
+		.build();
 
-    public void start() {
-		ModelBuilder modelBuilder = Model.builder();
-		Model model = modelBuilder.useCase("Measure performance of simple mathematical operations")
-			.basicFlow()
-				.step("S1").system(this::addsTwoNumbers)
-				.step("S2").system(this::calculatesTwoToThePowerOfAThousand)
-				.step("S3").system(this::displaysResults).build();
-	
 		ModelRunner modelRunner = new ModelRunner();
-		modelRunner.handleWith(this::measuresPerformance).run(model);
-    }
+		modelRunner.handleWith(this::measuresPerformance);
+		modelRunner.run(model).reactTo(new RequestCalculating());
+	}
 
-    private void measuresPerformance(StepToBeRun stepToBeRun) {
-	long timeBefore = System.nanoTime();
-	stepToBeRun.run();
-	long timeAfter = System.nanoTime();
-	long timeElapsed = timeAfter - timeBefore;
+	private void measuresPerformance(StepToBeRun stepToBeRun) {
+		long timeBefore = System.nanoTime();
+		stepToBeRun.run();
+		long timeAfter = System.nanoTime();
+		long timeElapsed = timeAfter - timeBefore;
 
-	System.out.println("Step " + stepToBeRun.getStepName() + " took " + timeElapsed + " nanoseconds.");
-    }
+		System.out.println("Elapsed time: " + timeElapsed + " nanoseconds.");
+	}
 
-    private void addsTwoNumbers() {
-	resultOfAddition = 123456789 + 123456789;
-    }
+	private void calculate() {
+		Math.pow(2, 1000);
+	}
 
-    private void calculatesTwoToThePowerOfAThousand() {
-	resultOfPower = Math.pow(2, 1000);
-    }
+	public static void main(String[] args) {
+		new CrossCuttingConcerns01().start();
+	}
 
-    private void displaysResults() {
-	System.out.println("Result of addition: " + resultOfAddition);
-	System.out.println("Result of 2 to the power of a thousand = " + resultOfPower);
-    }
-
-    public static void main(String[] args) {
-	new CrossCuttingConcerns01().start();
-    }
+	private class RequestCalculating {
+	}
 }
