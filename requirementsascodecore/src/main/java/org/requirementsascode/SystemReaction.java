77f6e0f80c2b0778271e7fb546a0c85a9defe06f
b_muth <Bertil.Muth@HOOD-Group.com>
2019-09-21 10:30:57 +0200

    Refactor SystemReaction class

--- requirementsascodecore/src/main/java/org/requirementsascode/SystemReaction.java
+++ requirementsascodecore/src/main/java/org/requirementsascode/SystemReaction.java
@@ -5,55 +5,55 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+/**
+ * An instance of this class represents an element specified via .system(..) by the user of the library.
+ * That element is called modelObject.
+ * 
+ * Internally, each system reaction needs to be represented as an function, even if it has been specified as
+ * a consumer by the user of the library. This approach simplifies development of the rest of the API.
+ * 
+ * @author b_muth
+ *
+ * @param <T> the kind of message that is the input for this system reactions
+ */
 public class SystemReaction<T> implements Function<T, Object> {
-	private Object system;
+	private Object modelObject;
 	private Function<T, Object> internalFunction;
 
-	SystemReaction(Consumer<T> system) {
-		this.system = Objects.requireNonNull(system);
+	SystemReaction(Consumer<T> modelObject) {
+		this.modelObject = Objects.requireNonNull(modelObject);
 		
 		Function<T, Object> nonPublishingReaction = message -> {
-			system.accept(message);
+			modelObject.accept(message);
 			return null;
 		};
 		this.internalFunction = nonPublishingReaction;
 	}
-	
-	SystemReaction(Consumer<T> system, Function<T, Object> publish) {
-		this.system = Objects.requireNonNull(system);
-		
-		Function<T, Object> nonPublishingReaction = message -> {
-			system.accept(message);
-			Object returnedEvent = publish.apply(message);
-			return returnedEvent;
-		};
-		this.internalFunction = nonPublishingReaction;
+
+	SystemReaction(Runnable modelObject) {
+		this((Consumer<T>) ignoredRunner -> modelObject.run());
+		this.modelObject = modelObject;
 	}
 
-	SystemReaction(Runnable system) {
-		this((Consumer<T>) ignoredRunner -> system.run());
-		this.system = system;
-	}
-
-	SystemReaction(Supplier<Object> system) {
-		this.system = Objects.requireNonNull(system);
+	SystemReaction(Supplier<Object> modelObject) {
+		this.modelObject = Objects.requireNonNull(modelObject);
 		
-		Function<T, Object> publishingReaction = (Function<T, Object>) message -> system.get();
+		Function<T, Object> publishingReaction = (Function<T, Object>) message -> modelObject.get();
 		this.internalFunction = publishingReaction;
 	}
 
 	SystemReaction(Function<T, Object> modelObject) {
 		Objects.requireNonNull(modelObject);
-		this.system = modelObject;
+		this.modelObject = modelObject;
 		this.internalFunction = modelObject;
 	}
 
 	public Object getModelObject() {
-		return system; 
+		return modelObject; 
 	}
 
 	@Override
-	public Object apply(T event) {
-		return internalFunction.apply(event);
+	public Object apply(T message) {
+		return internalFunction.apply(message);
 	}
 }
