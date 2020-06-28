86f07c96c7f1142be84ab0b80cb21b60a10f9c89
bertilmuth <Bertil.Muth@HOOD-Group.com>
2020-06-28 23:45:30 +0200

    Add test for publishing to specific recipient

--- requirementsascodecore/src/test/java/org/requirementsascode/NonStandardEventHandlingTest.java
+++ requirementsascodecore/src/test/java/org/requirementsascode/NonStandardEventHandlingTest.java
@@ -112,7 +112,7 @@ public class NonStandardEventHandlingTest extends AbstractTestCase {
 	}
 
 	@Test
-	public void publishesEventToList() {
+	public void publishesEvent() {
 		List<Object> publishedEvents = new ArrayList<>();
 		
 		Model model = modelBuilder
@@ -127,6 +127,29 @@ public class NonStandardEventHandlingTest extends AbstractTestCase {
 
 		assertEquals(1, publishedEvents.size());
 		assertEquals(EntersText.class, publishedEvents.get(0).getClass());
+		assertEquals("S2", modelRunner.getLatestStep().get().getName());
+	}
+	
+	@Test
+	public void publishesEventToActor() {
+		List<Object> actualRecipients = new ArrayList<>();
+		Actor recipient = new Actor("Recipient");
+		
+		Model model = modelBuilder
+			.useCase(USE_CASE)
+				.basicFlow()
+					.step("S1").on(EntersText.class).systemPublish(super.publishesEnteredTextAsEvent()).to(recipient)
+					.step("S2").on(String.class).system(new IgnoresIt<>())
+				.build();
+
+		modelRunner.publishWith(event -> {
+			Step step = modelRunner.getLatestStep().get();
+			step.getPublishTo().ifPresent(to -> actualRecipients.add(to));
+		});
+		modelRunner.run(model).reactTo(entersText(), "Some String");
+
+		assertEquals(1, actualRecipients.size());
+		assertEquals(recipient, actualRecipients.get(0));
 		assertEquals("S2", modelRunner.getLatestStep().get().getName());
 	}
 }
