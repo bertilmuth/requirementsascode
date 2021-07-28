3027e754d63b267c2671e9a91b75133e7e246fbe
bertilmuth <bertilmuth@yahoo.de>
2021-07-28 12:35:42 +0200

    Fix ExceptionsThrownTest

--- requirementsascodecore/src/test/java/org/requirementsascode/ExceptionsThrownTest.java
+++ requirementsascodecore/src/test/java/org/requirementsascode/ExceptionsThrownTest.java
@@ -1,9 +1,9 @@
 package org.requirementsascode;
 
-import org.junit.Rule;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.rules.ExpectedException;
 import org.requirementsascode.exception.ElementAlreadyInModel;
 import org.requirementsascode.exception.InfiniteRepetition;
 import org.requirementsascode.exception.MissingUseCaseStepPart;
@@ -12,9 +12,6 @@ import org.requirementsascode.exception.NestedCallOfReactTo;
 import org.requirementsascode.exception.NoSuchElementInModel;
 
 public class ExceptionsThrownTest extends AbstractTestCase {
-	@Rule
-	public ExpectedException thrown = ExpectedException.none();
-
 	@BeforeEach
 	public void setup() {
 		setupWithRecordingModelRunner();
@@ -22,182 +19,168 @@ public class ExceptionsThrownTest extends AbstractTestCase {
 
   @Test
   public void throwsExceptionIfInsteadOfStepNotExistsInSameUseCase() {
-    thrown.expect(NoSuchElementInModel.class);
-    thrown.expectMessage(CUSTOMER_ENTERS_TEXT);
-
-    modelBuilder
-      .useCase(USE_CASE)
-        .basicFlow().insteadOf(CUSTOMER_ENTERS_TEXT)
-          .step("S1").system(displaysConstantText())
-        .build();
+    assertThrows(NoSuchElementInModel.class, () -> {
+      modelBuilder
+        .useCase(USE_CASE)
+          .basicFlow().insteadOf(CUSTOMER_ENTERS_TEXT)
+            .step("S1").system(displaysConstantText())
+          .build();
+    }, CUSTOMER_ENTERS_TEXT);
   }
 
 	@Test
 	public void throwsExceptionIfAfterStepNotExistsInSameUseCase() {
-		thrown.expect(NoSuchElementInModel.class);
-		thrown.expectMessage(CUSTOMER_ENTERS_TEXT);
-		
-    modelBuilder
-    .useCase(USE_CASE)
-      .basicFlow().after(CUSTOMER_ENTERS_TEXT)
-        .step("S1").system(displaysConstantText())
-      .build();
+    assertThrows(NoSuchElementInModel.class, () -> {
+      modelBuilder
+      .useCase(USE_CASE)
+        .basicFlow().after(CUSTOMER_ENTERS_TEXT)
+          .step("S1").system(displaysConstantText())
+        .build();
+    }, CUSTOMER_ENTERS_TEXT);
 	}
 
 	@Test
 	public void throwsExceptionIfContinueAfterNotExists() {
-		thrown.expect(NoSuchElementInModel.class);
-		thrown.expectMessage(CONTINUE);
-
-    modelBuilder
-    .useCase(USE_CASE)
-      .basicFlow()
-        .step("S1").continuesAfter(CONTINUE)
-      .build();
+		assertThrows(NoSuchElementInModel.class, () -> {
+      modelBuilder
+      .useCase(USE_CASE)
+        .basicFlow()
+          .step("S1").continuesAfter(CONTINUE)
+        .build();
+    }, CONTINUE);
 	}
 
 	@Test
 	public void throwsExceptionIfContinueAtNotExists() {
-		thrown.expect(NoSuchElementInModel.class);
-		thrown.expectMessage(CONTINUE);
-		
-    modelBuilder
-    .useCase(USE_CASE)
-      .basicFlow()
-        .step("S1").continuesAt(CONTINUE)
-      .build();
+    assertThrows(NoSuchElementInModel.class, () -> {
+      modelBuilder
+      .useCase(USE_CASE)
+        .basicFlow()
+          .step("S1").continuesAt(CONTINUE)
+        .build();
+    }, CONTINUE);
 	}
 
 	@Test
 	public void throwsExceptionIfContinueWithoutAlternativeAtNotExists() {
-		thrown.expect(NoSuchElementInModel.class);
-		thrown.expectMessage(CONTINUE);
-		
-    modelBuilder.useCase(USE_CASE)
-      .basicFlow()
-        .step("S1").continuesAt(CONTINUE)
-      .build();
+    assertThrows(NoSuchElementInModel.class, () -> {
+      modelBuilder.useCase(USE_CASE)
+        .basicFlow()
+          .step("S1").continuesAt(CONTINUE)
+        .build();
+    }, CONTINUE);
 	}
 
 	@Test
 	public void throwsExceptionIfFlowIsCreatedTwice() {
-		thrown.expect(ElementAlreadyInModel.class);
-		thrown.expectMessage(ALTERNATIVE_FLOW);
-
-		modelBuilder.useCase(USE_CASE)
-			.flow(ALTERNATIVE_FLOW)
-				.step(SYSTEM_DISPLAYS_TEXT).system(displaysConstantText())
-			.flow(ALTERNATIVE_FLOW)
-			  .step(SYSTEM_DISPLAYS_TEXT_AGAIN).system(displaysConstantText())
-			.build();
+		assertThrows(ElementAlreadyInModel.class, () -> {
+  		modelBuilder.useCase(USE_CASE)
+  			.flow(ALTERNATIVE_FLOW)
+  				.step(SYSTEM_DISPLAYS_TEXT).system(displaysConstantText())
+  			.flow(ALTERNATIVE_FLOW)
+  			  .step(SYSTEM_DISPLAYS_TEXT_AGAIN).system(displaysConstantText())
+  			.build();
+    }, ALTERNATIVE_FLOW);
 	}
 
 	@Test
 	public void throwsExceptionIfStepIsCreatedTwice() {
-		thrown.expect(ElementAlreadyInModel.class);
-		thrown.expectMessage(CUSTOMER_ENTERS_TEXT);
-
-		modelBuilder.useCase(USE_CASE)
-			.basicFlow()
-				.step(CUSTOMER_ENTERS_TEXT).system(displaysConstantText())
-				.step(CUSTOMER_ENTERS_TEXT).system(displaysConstantText())
-			.build();
+    assertThrows(ElementAlreadyInModel.class, () -> {
+  		modelBuilder.useCase(USE_CASE)
+  			.basicFlow()
+  				.step(CUSTOMER_ENTERS_TEXT).system(displaysConstantText())
+  				.step(CUSTOMER_ENTERS_TEXT).system(displaysConstantText())
+  			.build();
+    }, CUSTOMER_ENTERS_TEXT);
 	}
 
 	@Test
 	public void throwsExceptionWhenConditionIsAlwaysTrue() {
-		thrown.expect(InfiniteRepetition.class);
-		thrown.expectMessage("S1");
-
-		Model model = modelBuilder
-		  .condition(() -> true).system(() -> {})
-		.build();
-
-		modelRunner.run(model);
+    assertThrows(InfiniteRepetition.class, () -> {
+  		Model model = modelBuilder
+  		  .condition(() -> true).system(() -> {})
+  		.build();
+  		
+      modelRunner.run(model);
+    }, "S1");
 	}
 
   @Test
   public void throwsExceptionWhenReactToIsCalledFromSystemReaction() {
-    thrown.expect(NestedCallOfReactTo.class);
-
-    Model model = modelBuilder.useCase(USE_CASE)
-      .basicFlow()
-        .step(CUSTOMER_ENTERS_TEXT).system(() -> modelRunner.reactTo(""))
-    .build();
-
-    modelRunner.run(model);
+    assertThrows(NestedCallOfReactTo.class, () -> {
+      Model model = modelBuilder.useCase(USE_CASE)
+        .basicFlow()
+          .step(CUSTOMER_ENTERS_TEXT).system(() -> modelRunner.reactTo(""))
+      .build();
+  
+      modelRunner.run(model);
+    });
   }
 
   @Test
   public void throwsExceptionIfMoreThanOneStepCanReactInSameUseCase() {
-    thrown.expect(MoreThanOneStepCanReact.class);
-    thrown.expectMessage(CUSTOMER_ENTERS_TEXT);
-    thrown.expectMessage(CUSTOMER_ENTERS_ALTERNATIVE_TEXT);
-
-		Model model = modelBuilder.useCase(USE_CASE)
-			.basicFlow().anytime()
-				.step(CUSTOMER_ENTERS_TEXT).system(displaysConstantText())
-			.flow(ALTERNATIVE_FLOW).anytime()
-				.step(CUSTOMER_ENTERS_ALTERNATIVE_TEXT).system(displaysConstantText())
-			.build();
-
-    modelRunner.run(model);
+    assertThrows(MoreThanOneStepCanReact.class, () -> {
+  		Model model = modelBuilder.useCase(USE_CASE)
+  			.basicFlow().anytime()
+  				.step(CUSTOMER_ENTERS_TEXT).system(displaysConstantText())
+  			.flow(ALTERNATIVE_FLOW).anytime()
+  				.step(CUSTOMER_ENTERS_ALTERNATIVE_TEXT).system(displaysConstantText())
+  			.build();
+  
+      modelRunner.run(model);
+    }, CUSTOMER_ENTERS_TEXT);
   }
 
 	@Test
 	public void throwsExceptionIfMoreThanOneStepCanReactInDifferentUseCases() {
-		thrown.expect(MoreThanOneStepCanReact.class);
-		thrown.expectMessage("Step 1");
-		thrown.expectMessage("Step 2 with same event as Step 1");
-
-		Model model = modelBuilder
-			.useCase("Use Case")
-				.basicFlow()
-					.step("Step 1").user(String.class).system(s -> System.out.println(s))
-			.useCase("Another Use Case")
-				.basicFlow()
-					.step("Step 2 with same event as Step 1").user(String.class).system(s -> System.out.println(s))
-			.build();
-
-		modelRunner.run(model);
-		modelRunner.reactTo("Some text");
+		assertThrows(MoreThanOneStepCanReact.class, () -> {
+  		Model model = modelBuilder
+  			.useCase("Use Case")
+  				.basicFlow()
+  					.step("Step 1").user(String.class).system(s -> System.out.println(s))
+  			.useCase("Another Use Case")
+  				.basicFlow()
+  					.step("Step 2 with same event as Step 1").user(String.class).system(s -> System.out.println(s))
+  			.build();
+  
+  		modelRunner.run(model);
+  		modelRunner.reactTo("Some text");
+    }, "Step 2 with same event as Step 1");
 	}
 
 	@Test
 	public void throwsExceptionIfActorPartIsNotSpecified() {
-		thrown.expect(MissingUseCaseStepPart.class);
-		thrown.expectMessage(CUSTOMER_ENTERS_TEXT);
-
-		modelBuilder.useCase(USE_CASE).basicFlow().step(CUSTOMER_ENTERS_TEXT);
-
-		Model model = modelBuilder.build();
-
-		modelRunner.run(model);
+    assertThrows(MissingUseCaseStepPart.class, () -> {
+  		modelBuilder.useCase(USE_CASE).basicFlow().step(CUSTOMER_ENTERS_TEXT);
+  
+  		Model model = modelBuilder.build();
+  
+  		modelRunner.run(model);
+    }, CUSTOMER_ENTERS_TEXT);
 	}
 
 	@Test
 	public void throwsExceptionIfSystemPartIsNotSpecified() {
-		thrown.expect(MissingUseCaseStepPart.class);
-		thrown.expectMessage(CUSTOMER_ENTERS_TEXT);
-
-		modelBuilder.useCase(USE_CASE).basicFlow().step(CUSTOMER_ENTERS_TEXT).as(customer).user(EntersText.class);
-
-		Model model = modelBuilder.build();
-
-		modelRunner.as(customer).run(model);
-		modelRunner.reactTo(entersText());
+		assertThrows(MissingUseCaseStepPart.class, () -> {
+  		modelBuilder.useCase(USE_CASE).basicFlow().step(CUSTOMER_ENTERS_TEXT).as(customer).user(EntersText.class);
+  
+  		Model model = modelBuilder.build();
+  
+  		modelRunner.as(customer).run(model);
+  		modelRunner.reactTo(entersText());
+    }, CUSTOMER_ENTERS_TEXT);
 	}
 
 	@Test
-	public void rethrowsExceptionIfExceptionIsNotHandled() {
-		thrown.expect(IllegalStateException.class);
-
-		modelBuilder.useCase(USE_CASE)
-		  .basicFlow()
-		    .step(CUSTOMER_ENTERS_TEXT).system(() -> {throw new IllegalStateException();});
-
-		Model model = modelBuilder.build();
-
-		modelRunner.run(model);
+	public void rethrowsExceptionIfExceptionIsNotHandled() {		
+		assertThrows(IllegalStateException.class, () -> {
+  		modelBuilder.useCase(USE_CASE)
+  		  .basicFlow()
+  		    .step(CUSTOMER_ENTERS_TEXT).system(() -> {throw new IllegalStateException();});
+  
+  		Model model = modelBuilder.build();
+  
+  		modelRunner.run(model);
+    });
 	}
 }
