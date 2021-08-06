5b7188eb4c346754eb646252c1f7ce6859e725a2
Bertil Muth <bertilmuth@yahoo.de>
2021-08-06 14:54:15 +0200

    Improve layout

--- requirementsascodecore/src/main/java/org/requirementsascode/BehaviorModel.java
+++ requirementsascodecore/src/main/java/org/requirementsascode/BehaviorModel.java
@@ -8,17 +8,17 @@ package org.requirementsascode;
  * 
  */
 public interface BehaviorModel {
-	/**
-	 * A model that defines which messages are consumed and which responses are
-	 * produced. See See
-	 * <a href="https://github.com/bertilmuth/requirementsascode">the requirements
-	 * as code website</a> for examples.
-	 * 
-	 * @return the behavior model
-	 */
-	Model model();
+  /**
+   * A model that defines which messages are consumed and which responses are
+   * produced. See See
+   * <a href="https://github.com/bertilmuth/requirementsascode">the requirements
+   * as code website</a> for examples.
+   * 
+   * @return the behavior model
+   */
+  Model model();
 
-	/**
+  /**
    * The default response is the response that a behavior returns when a message
    * is just consumed (via a <code>.system(...)</code> definition in the model),
    * or when a handler function returns null.
